4e7852b6872ab2de3732c5859f35567fa606a936
Athina.K.A <140839336+Athina1509@users.noreply.github.com>
2023-07-29 13:28:45 +0530

    Add files via upload

---
+++ simple.calculator.py
@@ -0,0 +1,58 @@
+#**//Simple Calculator//**#
+# This function adds two numbers
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+    #Enter any one of the number in foure options
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+    #If next calculation want to be continue??
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          exit()
+        
+    else:
+        print("continue")
